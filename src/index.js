c44e4a38d615097a11cc712a0e708a4b83eac4f5
Artemiy <rendrom@gmail.com>
2021-05-06 22:32:08 +0800

    feat: add OL example with ngw select by bbox

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { DragBox, Select } from 'ol/interaction.js';
 import { platformModifierKeyOnly } from 'ol/events/condition.js';
+import GeoJSON from 'ol/format/GeoJSON';
 import NgwMap from '@nextgis/ngw-ol';
+import { fetchNgwLayerFeatures } from '@nextgis/ngw-kit';
 
 NgwMap.create({
   target: 'map',
@@ -12,8 +14,13 @@ NgwMap.create({
   },
 }).then((ngwMap) => {
   ngwMap.addBaseLayer('OSM');
+  // selectFromVectorLayer(4253, ngwMap);
+  selectFromTileLayer(4253, ngwMap);
+});
+
+function selectFromVectorLayer(resource, ngwMap) {
   const map = ngwMap.mapAdapter.map;
-  ngwMap.addNgwLayer({ resource: 4253, fit: true }).then((adapter) => {
+  ngwMap.addNgwLayer({ resource, fit: true }).then((adapter) => {
     const layer = adapter.layer;
     const vectorSource = layer.getSource();
     const select = new Select();
@@ -29,8 +36,7 @@ NgwMap.create({
     dragBox.on('boxend', function () {
       // features that intersect the box are added to the collection of
       // selected features
-      var extent = dragBox.getGeometry().getExtent();
-      console.log(extent);
+      const extent = dragBox.getGeometry().getExtent();
       vectorSource.forEachFeatureIntersectingExtent(extent, function (feature) {
         selectedFeatures.push(feature);
       });
@@ -41,6 +47,50 @@ NgwMap.create({
       selectedFeatures.clear();
     });
   });
+}
 
-  // ngwMap.addControl(measureControl, 'top-right');
-});
+function selectFromTileLayer(resource, ngwMap) {
+  const map = ngwMap.mapAdapter.map;
+  ngwMap
+    .addNgwLayer({ resource, fit: true, adapter: 'TILE' })
+    .then((adapter) => {
+      const layer = adapter.layer;
+      const vectorSource = layer.getSource();
+      const select = new Select();
+      map.addInteraction(select);
+
+      ngwMap.addGeoJsonLayer({ id: 'highlight' });
+
+      const dragBox = new DragBox({
+        condition: platformModifierKeyOnly,
+      });
+      map.addInteraction(dragBox);
+
+      dragBox.on('boxend', function () {
+        const e = dragBox.getGeometry().getExtent();
+        const polygon = [
+          [e[0], e[1]],
+          [e[0], e[3]],
+          [e[2], e[3]],
+          [e[2], e[1]],
+          [e[0], e[1]],
+        ].map(([x, y]) => x + ' ' + y);
+        const wkt = `POLYGON((${polygon.join(', ')}))`;
+        fetchNgwLayerFeatures({
+          resourceId: resource,
+          connector: ngwMap.connector,
+          intersects: wkt,
+        }).then((features) => {
+          ngwMap.setLayerData('highlight', {
+            type: 'FeatureCollection',
+            features,
+          });
+        });
+      });
+
+      // clear selection when drawing a new box and when clicking on the map
+      dragBox.on('boxstart', function () {
+        ngwMap.clearLayerData('highlight');
+      });
+    });
+}
