4814b12a399eb87df52fbf621097e6fabc25a6dc
Artemiy <rendrom@gmail.com>
2021-08-26 20:53:41 +0200

    build: update webpack config, add polyfils

--- src/index.js
+++ src/index.js
@@ -1,116 +1,24 @@
-import NgwMap from '@nextgis/ngw-ol';
-import { DragBox, Select } from 'ol/interaction.js';
-import { platformModifierKeyOnly } from 'ol/events/condition.js';
-import { getBoundsCoordinates } from '@nextgis/utils';
-import { fetchNgwLayerFeatures } from '@nextgis/ngw-kit';
+import NgwMap from '@nextgis/ngw-leaflet';
 
 NgwMap.create({
   target: 'map',
-  baseUrl: 'https://demo.nextgis.com/',
-  // center: [104, 52],
-  // zoom: 6,
-  mapAdapterOptions: {
-    //
-  },
+  baseUrl: 'https://demo.nextgis.com',
+  zoom: 6,
 }).then((ngwMap) => {
-  ngwMap.addBaseLayer('OSM');
-  selectFromTileLayer(4253, ngwMap);
-});
+  const connector = ngwMap.connector;
 
-function selectFromVectorLayer(resource, ngwMap) {
-  const map = ngwMap.mapAdapter.map;
-  ngwMap
-    .addNgwLayer({
-      resource,
+  connector.getResource(5301).then((x) => {
+    console.log(x);
+  });
+
+  ngwMap.addNgwLayer({
+    id: 'building',
+    resource: 5300,
+    adapterOptions: {
       fit: true,
-      adapterOptions: {
-        paint: {
-          color: 'white',
-          stroke: true,
-          weight: 3,
-          strokeColor: 'blue',
-          fillOpacity: 1,
-          radius: 5,
-        },
-      },
-    })
-    .then((adapter) => {
-      const layer = adapter.layer;
-      const vectorSource = layer.getSource();
-      const select = new Select();
-      map.addInteraction(select);
-
-      const selectedFeatures = select.getFeatures();
-
-      const dragBox = new DragBox({
-        condition: platformModifierKeyOnly,
-      });
-      map.addInteraction(dragBox);
-
-      dragBox.on('boxend', function () {
-        // features that intersect the box are added to the collection of
-        // selected features
-        const extent = dragBox.getGeometry().getExtent();
-        vectorSource.forEachFeatureIntersectingExtent(
-          extent,
-          function (feature) {
-            selectedFeatures.push(feature);
-          }
-        );
-      });
-
-      // clear selection when drawing a new box and when clicking on the map
-      dragBox.on('boxstart', function () {
-        selectedFeatures.clear();
-      });
-    });
-}
-
-function selectFromTileLayer(resource, ngwMap) {
-  const map = ngwMap.mapAdapter.map;
-  ngwMap
-    .addNgwLayer({ resource, fit: true, adapter: 'TILE' })
-    .then((adapter) => {
-      const layer = adapter.layer;
-      const vectorSource = layer.getSource();
-      const select = new Select();
-      map.addInteraction(select);
-
-      ngwMap.addGeoJsonLayer({
-        id: 'highlight',
-        paint: {
-          color: 'white',
-          stroke: true,
-          weight: 3,
-          strokeColor: 'blue',
-          radius: 8,
-        },
-      });
-
-      const dragBox = new DragBox({
-        condition: platformModifierKeyOnly,
-      });
-      map.addInteraction(dragBox);
-
-      dragBox.on('boxend', function () {
-        const e = dragBox.getGeometry().getExtent();
-        const polygon = getBoundsCoordinates(e).map(([x, y]) => x + ' ' + y);
-        const wkt = `POLYGON((${polygon.join(', ')}))`;
-        fetchNgwLayerFeatures({
-          resourceId: resource,
-          connector: ngwMap.connector,
-          intersects: wkt,
-        }).then((features) => {
-          ngwMap.setLayerData('highlight', {
-            type: 'FeatureCollection',
-            features,
-          });
-        });
-      });
-
-      // clear selection when drawing a new box and when clicking on the map
-      dragBox.on('boxstart', function () {
-        ngwMap.clearLayerData('highlight');
-      });
-    });
-}
+      unselectOnSecondClick: true,
+      paint: { color: 'brown' },
+      selectedPaint: { color: 'red' },
+    },
+  });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,36 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: '/src/index.js',
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
+
+  return {
+    mode: argv.mode || 'development',
+    entry: '/src/index.js',
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
           },
         },
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.png/,
-        type: 'asset/resource',
-      },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.png/,
+          type: 'asset/resource',
+        },
+      ],
+    },
+    target: isProd ? 'browserslist' : 'web',
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+      }),
     ],
-  },
-  target: ['es2015', 'web'],
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-    }),
-  ],
+  };
 };
